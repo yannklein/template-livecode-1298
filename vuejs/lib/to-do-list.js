072603938afcf6f17ee9c444fc6126f6003b07c1
Yann Klein <yann.klein@me.com>
2023-08-02 18:10:56 +0900

    liveccode done

--- vuejs/lib/to-do-list.js
+++ vuejs/lib/to-do-list.js
@@ -3,7 +3,29 @@ import { createApp } from "vue";
 createApp({
   data() {
     return {
-      message: "If you see this message in your browswer, that means Vue is successfully mounted! 🙌"
+      todos: [],
+      newTodo: null
+    }
+  },
+  methods: {
+    addTodo() {
+      console.log("add!");
+      this.todos.push({
+        title: this.newTodo,
+        done: false
+      });
+    }
+  },
+  mounted() {
+    this.todos = JSON.parse(localStorage.getItem("todos")) || []
+  },
+  watch: {
+    todos: {
+      handler() {
+        console.log("watch");
+        localStorage.setItem("todos", JSON.stringify(this.todos))
+      },
+      deep: true
     }
   }
 }).mount('#todosContainer');
